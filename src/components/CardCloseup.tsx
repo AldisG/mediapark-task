bd95d7dbe07dafb8d4d63d520d141ba93cd5fce4
AldisG <aldisgailis1@gmail.com>
2022-10-07 20:33:28 +0300

    fixup! refactored photocards and closeUp card

--- src/components/CardCloseup.tsx
+++ src/components/CardCloseup.tsx
@@ -20,7 +20,7 @@ const CardCloseup: FC<P> = ({ item, openWindow, handleCloseWindow }) => {
   const { description, urls, alt_description, id } = item;
 
   return (
-    <>
+    <Box>
       <Dialog
         open={openWindow}
         aria-labelledby="alert-dialog-title"
@@ -37,43 +37,45 @@ const CardCloseup: FC<P> = ({ item, openWindow, handleCloseWindow }) => {
             position: "relative",
           }}
         >
-          {description && (
-            <Typography variant="h4" fontWeight="bold">
-              {description}
-            </Typography>
-          )}
-          {alt_description && (
-            <Typography variant="body2">{alt_description}</Typography>
-          )}
+          <Box sx={{ maxWidth: "90%" }}>
+            {description && (
+              <Typography variant="h4" fontWeight="bold">
+                {description}
+              </Typography>
+            )}
+            {alt_description && (
+              <Typography variant="body2">{alt_description}</Typography>
+            )}
+          </Box>
           <Card elevation={5}>
-              <CardMedia
-                component="img"
-                height="auto"
-                image={urls.regular}
-                alt={alt_description || `photo ID: ${id}`}
-              />
+            <CardMedia
+              component="img"
+              height="auto"
+              image={urls.regular}
+              alt={alt_description || `photo ID: ${id}`}
+            />
           </Card>
 
           <Typography variant="body2">
             To downlad - rightclick on photo and select "Save image as"
           </Typography>
-          <Button
-            sx={{
-              fontWeight: "bold",
-              position: "absolute",
-              top: 0,
-              right: 0,
-              borderRadius: "100px",
-            }}
-            onClick={handleCloseWindow}
-            color="secondary"
-            autoFocus
-          >
-            <AiFillCloseCircle size={50} />
-          </Button>
         </Box>
+        <Button
+          sx={{
+            fontWeight: "bold",
+            position: "absolute",
+            top: 0,
+            right: 0,
+            borderRadius: "100px",
+          }}
+          onClick={handleCloseWindow}
+          color="secondary"
+          autoFocus
+        >
+          <AiFillCloseCircle size={50} />
+        </Button>
       </Dialog>
-    </>
+    </Box>
   );
 };
 
